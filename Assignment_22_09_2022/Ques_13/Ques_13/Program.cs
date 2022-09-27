40842963a79bd6375310380fc172dd7fbf8acaa7
Mukesh Lodha <Mukesh.Lodha@ivycomptech.com>
2022-09-27 21:01:35 +0530

    Add Assignment od 22 sep

---
+++ Assignment_22_09_2022/Ques_13/Ques_13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ques_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "MUKESH kumar LOdha";
+
+            int len = str1.Length;
+
+            string str2 = " ";
+            int i = 0;
+            while ( i< len)
+            {
+                if (Char.IsLower(str1[i]))
+                {
+                   str2 = str2 + Char.ToUpper(str1[i]);
+                }else if (Char.IsUpper(str1[i]))
+                {
+                   str2 = str2 + Char.ToLower(str1[i]);
+                }
+                else
+                {
+                    str2 = str2 + " ";
+                }
+                i++;
+            }
+            Console.WriteLine(str2);
+        }
+    }
+}
---
+++ Assignment_22_09_2022/Ques_14/Ques_14/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ques_14
+{
+    internal class Program
+    {
+        public static int length_Of_Longest_Substring(String s)
+        {
+            int length = 0;
+            for (int start = 0; start < s.Length; start++)
+            {
+                int i = start;
+                var dictionary = new Dictionary<char, int>();
+
+                while (i < s.Length && !dictionary.ContainsKey(s[i])) 
+                { 
+                    dictionary.Add(s[i++], 1);
+                }
+                if (i - start > length)
+                {
+                    length = i - start;
+                }
+            }
+
+            return length;
+        }
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            int len = length_Of_Longest_Substring(str);
+
+            Console.WriteLine(len);
+        }
+
+
+    }
+}
